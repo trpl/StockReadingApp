93f21f97b8141bcdd0af974229a8dc2b556622bb
Nicolas SZYMENDERA <szymenderanicolas@gmail.com>
2020-09-12 16:26:03 +0200

    Add StockReadingListingView add template + empty css

--- StockReadingApp/StockReadingApp/urls.py
+++ StockReadingApp/StockReadingApp/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from stockreading.views import (
+    CurrentListingView,
+    HistoricListingView
+)
+
 urlpatterns = [
+    path('', CurrentListingView.as_view(), name='current-listing'),
+    path('historic/', HistoricListingView.as_view(), name='historic-listing'),
     path('admin/', admin.site.urls),
 ]
--- StockReadingApp/stockreading/views.py
+++ StockReadingApp/stockreading/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import StockReading
+
+class StockReadingListingView(ListView):
+    model = StockReading
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        context = super(StockReadingListingView, self).get_context_data(**kwargs)
+        context['reference_id'] = self.request.GET.get('reference_id')
+        return context
+
+
+class CurrentListingView(StockReadingListingView):
+    template_name = 'current_listing.html'
+    
+    def get_queryset(self):
+        kwargs = dict()
+        reference_id = self.request.GET.get('reference_id')
+        if reference_id:
+            kwargs["reference_id"] = reference_id
+        return StockReading.objects.filter_current(**kwargs).order_by(
+            'expiry_date', 'reference_id'
+        )
+
+
+class HistoricListingView(StockReadingListingView):
+    template_name = 'historic_listing.html'
+
+    def get_queryset(self):
+        kwargs = dict()
+        reference_id = self.request.GET.get('reference_id')
+        if reference_id:
+            kwargs["reference_id"] = reference_id
+        return StockReading.objects.filter_historic(**kwargs).order_by(
+            'expiry_date', 'reference_id'
+        )
